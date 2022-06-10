ef9a7d40d9b392e66e3b14326a5118c967da6fa1
EC2 Default User <ec2-user@ip-172-26-14-140.ec2.internal>
2022-06-10 06:49:55 +0000

    Insert user proof of concept

--- api.go
+++ api.go
@@ -4,7 +4,34 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
 )
+
+func addUser(w http.ResponseWriter, r *http.Request) {	
+	fmt.Println("Endpoint hit: addUser")
+	db, err := sql.Open("sqlite3", "../Prod.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	sqlStmt := `
+	insert into User (time_created)
+	values (datetime());
+	`
+	result, err := db.Exec(sqlStmt)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	fmt.Fprintf(w, "User added: %d", id)
+}
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the HomePage!")
@@ -13,6 +40,7 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 
 func handleRequests() {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/addUser", addUser)
 	fmt.Println("Listening at port 3000")
 	log.Fatal(http.ListenAndServe(":3000", nil))
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module rubiks-api
+
+go 1.16
+
+require github.com/mattn/go-sqlite3 v1.14.13 // indirect
