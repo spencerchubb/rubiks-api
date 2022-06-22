78b60cd48f75eadc58372c855f073e2eab3b6fa1
Spencer Chubb <spencerchubb@gmail.com>
2022-06-22 06:40:01 +0000

    Fix addAnalyticsEvent

--- api.go
+++ api.go
@@ -5,30 +5,22 @@ import (
 	"encoding/json"
 	"fmt"
 	_ "github.com/mattn/go-sqlite3"
-	"io/ioutil"
 	"log"
 	"net/http"
 )
 
-func logEndpoint(endpoint string) {
-	fmt.Printf("Endpoint hit: %s\n", endpoint)
-}
-
-func open() *sql.DB {
+func open() (*sql.DB, error) {
 	return sql.Open("sqlite3", "../Prod.db")
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	logEndpoint("homePage")
 	fmt.Fprintf(w, "Welcome to the HomePage!")
 }
 
 func addUser(w http.ResponseWriter, r *http.Request) {
-	logEndpoint("addUser")
-
 	db, err := open()
 	if err != nil {
-		log.Fatal(err)
+		fmt.Printf("Error in addUser, open(): %s\n", err)
 		return
 	}
 	defer db.Close()
@@ -39,30 +31,22 @@ func addUser(w http.ResponseWriter, r *http.Request) {
 	`
 	result, err := db.Exec(stmt)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Printf("Error in addUser, db.Exec(): %s\n", err)
 		return
 	}
 	id, err := result.LastInsertId()
 	if err != nil {
-		log.Fatal(err)
+		fmt.Printf("Error in addUser, result.LastInsertId(): %s\n", err)
 		return
 	}
 	fmt.Fprintf(w, "User added: %d", id)
 }
 
 func addAnalyticsEvent(w http.ResponseWriter, r *http.Request) {
-	logEndpoint("addAnalyticsEvent")
-
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		fmt.Printf("could not read body: %s\n", err)
-		return
-	}
-
 	var data map[string]interface{}
-	err = json.Unmarshal([]byte(body), &data)
+	err := json.NewDecoder(r.Body).Decode(&data)
 	if err != nil {
-		fmt.Printf("Could not unmarshal json: %s\n", err)
+		fmt.Printf("Error in addAnalyticsEvent, Decode: %s\n", err)
 		return
 	}
 	userID := data["userID"]
@@ -70,7 +54,7 @@ func addAnalyticsEvent(w http.ResponseWriter, r *http.Request) {
 
 	db, err := open()
 	if err != nil {
-		log.Fatal(err)
+		fmt.Printf("Error in addAnalyticsEvent, open(): %s\n", err)
 		return
 	}
 	defer db.Close()
@@ -81,16 +65,29 @@ func addAnalyticsEvent(w http.ResponseWriter, r *http.Request) {
 	`
 	_, err = db.Exec(stmt, userID, eventType)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Printf("Error in addAnalyticsEvent, db.Exec(): %s\n", err)
 		return
 	}
 	fmt.Fprintf(w, `{"success": true}`)
 }
 
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Headers", "*")
+}
+
+func handleFunc(pattern string, handler func(http.ResponseWriter, *http.Request)) {
+	http.HandleFunc(pattern, func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("Endpoint hit: %s\n", pattern)
+		enableCors(&w)
+		handler(w, r)
+	})
+}
+
 func handleRequests() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/addUser", addUser)
-	http.HandleFunc("/addAnalyticsEvent", addAnalyticsEvent)
+	handleFunc("/", homePage)
+	handleFunc("/addUser", addUser)
+	handleFunc("/addAnalyticsEvent", addAnalyticsEvent)
 	fmt.Println("Listening at port 3000")
 	log.Fatal(http.ListenAndServe(":3000", nil))
 }
